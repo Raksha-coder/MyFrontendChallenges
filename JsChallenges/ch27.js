387d9f4faa8d938db703e04d31e7201f40e3df0a
raksha-coder <ku.rakshadilipjaiswal@gmail.com>
2025-02-05 11:20:28 +0530

    ch27

--- JsChallenges/ch27.js
+++ JsChallenges/ch27.js
@@ -5,3 +5,13 @@ same properties as the second one (which may also
 have additional properties).
 */
 
+const obj1 = {a:1,b:2,c:1};
+const obj2 = {a:1,b:1,c:1};
+const obj3 = {a:1,b:5,e:1};
+
+
+const equalObject = (obj1,obj2) => Object.keys(obj1).every(prop => obj2[prop]);
+console.log(equalObject(obj1,obj2));
+console.log(equalObject(obj1,obj3));
+
+
