6d05fbde68d8c7b42c5dd1924e3f009b46abe015
raksha-coder <ku.rakshadilipjaiswal@gmail.com>
2025-02-05 11:38:35 +0530

    ch28

---
+++ JsChallenges/ch28.js
@@ -0,0 +1,26 @@
+/*
+write a js program to convert a comma-separated value
+string to a 2D array.A new line indicates a new row 
+in the array.
+
+example :
+abc,def,ghi []
+jkl,mno,pqr []
+stu,vwx,yza []
+
+*/
+
+const str = `abc,def,ghi 
+jkl,mno,pqr 
+stu,vwx,yza`;
+
+let parseString = str.split("\n").map(val=> val.split(","));
+console.log(parseString);
+
+// o/p : 
+// [
+//   [ 'abc', 'def', 'ghi ' ],
+//   [ 'jkl', 'mno', 'pqr ' ],
+//   [ 'stu', 'vwx', 'yza' ]
+// ]
+
